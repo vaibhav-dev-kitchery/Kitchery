07221e5c1b38d61a36139d45739d4b479b9dd117
Vaibhav Anand Jadhav <jadhavvaibhav1827@gmail.com>
2024-08-31 16:35:59 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+
+import {
+  SafeAreaView,
+  Text,
+  View
+} from 'react-native';
+
+
+function App(){
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Kitchery !!</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+
+
+export default App;
